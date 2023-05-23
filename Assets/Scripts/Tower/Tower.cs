83c8e14f89fba4c6fa3788fb4d9c7ed7423f0c26
fishillness <fishillness@gmail.com>
2023-05-23 17:57:35 +0600

    Script "Tower" updated. Turrets no longer miss.

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -6,8 +6,10 @@ namespace TowerDefense
     public class Tower : MonoBehaviour
     {
         [SerializeField] private float m_Radius;
+        [SerializeField] private float m_Lead = 0.3f;
         private Turret[] turrets;
-        private Destructible target = null;
+        private Rigidbody2D target = null;
+
 
         private void Start()
         {
@@ -18,12 +20,13 @@ namespace TowerDefense
         {
             if (target)
             {
-                Vector2 targetVector = target.transform.position - transform.position;
-                if (targetVector.magnitude <= m_Radius)
+                if (Vector3.Distance(target.transform.position, transform.position) <= m_Radius)
                 {
+
                     foreach (var turret in turrets)
                     {
-                        turret.transform.up = targetVector;
+                        turret.transform.up = target.transform.position
+                            - turret.transform.position + (Vector3)target.velocity * m_Lead;
                         turret.Fire();
                     }
                 }
@@ -31,14 +34,13 @@ namespace TowerDefense
                 {
                     target = null;
                 }
-                
             }
             else
             {
                 var enter = Physics2D.OverlapCircle(transform.position, m_Radius);
                 if (enter)
                 {
-                    target = enter.transform.root.GetComponent<Destructible>();
+                    target = enter.transform.root.GetComponent<Rigidbody2D>();
                 }
             }
             
